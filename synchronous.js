83c39f9311ba96e1841829bdddf9496c5b3d5c92
vin <kelvinwanerer@gmail.com>
2024-12-18 19:56:09 +0700

    Praktikum 10

---
+++ synchronous.js
@@ -0,0 +1,9 @@
+console.log("Kelvin Membuka Bowser Google Chrome.");
+
+console.log(`
+Kelvin download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("kelvin membuka Youtube.");
